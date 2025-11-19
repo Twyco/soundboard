ab6d46d1f624ea96fb032a6c3184435777cffb38
twyco <twyco@icloud.com>
2025-11-19 20:31:13 +0100

    reigster cleanup

--- src/main/java/de/twyco/soundboard/Soundboard.java
+++ src/main/java/de/twyco/soundboard/Soundboard.java
@@ -1,15 +1,12 @@
 package de.twyco.soundboard;
 
 import de.twyco.soundboard.client.GlobalKeybinds;
-import de.twyco.soundboard.client.hud.SoundboardHudRenderer;
+import de.twyco.soundboard.client.hud.HudService;
 import de.twyco.soundboard.util.config.SoundboardConfig;
 import de.twyco.soundboard.util.keybinding.KeyComboManager;
 import de.twyco.soundboard.util.sound.SoundManager;
 import net.fabricmc.api.ModInitializer;
 
-import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
-import net.fabricmc.fabric.api.client.rendering.v1.hud.VanillaHudElements;
-import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,11 +21,7 @@ public class Soundboard implements ModInitializer {
         KeyComboManager.init();
         SoundManager.init();
         GlobalKeybinds.init();
-        HudElementRegistry.attachElementBefore(
-                VanillaHudElements.CHAT,
-                Identifier.of(Soundboard.MOD_ID, "sounds_hud"),
-                SoundboardHudRenderer::render
-        );
+        HudService.init();
     }
 
 }
